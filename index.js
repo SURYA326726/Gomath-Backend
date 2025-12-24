b478458aaa569be40610c9c9f8efaad4c63fa3cc
SURYA326726 <suryam726326@gmail.com>
2025-12-24 18:12:34 +0530

    new Changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path=require('path')
 
@@ -5,17 +6,19 @@ const cors=require('cors');
 const app = express();
 const DBconnection=require('./DBconnection/DBconfig')
 const productRouter=require('./Routes/productRoutes')
+const loginRouter = require('./Routes/loginRoutes');
 
 
 
 app.use('/productImages',express.static(path.join(__dirname,'productImages')))
 app.use(cors({
-  origin: "https://gomathtech.netlify.app",
+  origin: ["https://gomathtech.netlify.app", "http://localhost:4200"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 app.use(express.json());
 app.use(productRouter)
+app.use(loginRouter);
 
 
 app.listen(3000, () => {
